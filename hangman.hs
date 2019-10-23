ef8e46049eff12eecbd102bfc5cbc5ef126e725d
Ghettovich <geert.boeve92@gmail.com>
2019-10-23 23:08:43 +0200

    print out game state

--- hangman.hs
+++ hangman.hs
@@ -1,12 +1,20 @@
+data Hangman = Hangman { secret :: String, guesses:: String, guessesLeft :: Int}
+
+randomWord :: String
+randomWord = "schildpad"
+
+maxGuesses :: Int
+maxGuesses = 6
+
 drawStickFigure :: Int -> String
 drawStickFigure x = unlines $ case x of
-    0 -> ["=========", "|    |", "|   ", "|   ", "|   "]
-    1 -> ["=========",
+    6 -> ["=========", "|    |", "|   ", "|   ", "|   "]
+    5 -> ["=========",
           "|    |",
           "|   " ++ " O ",
           "|   ",
           "|   "]
-    2 -> ["=========",
+    4 -> ["=========",
           "|    |",
           "|   " ++ " O ",
           "|   " ++ " |  "]
@@ -15,20 +23,42 @@ drawStickFigure x = unlines $ case x of
           "|   " ++ " O ",
           "|   " ++ " | ",
           "|   " ++ "/  "]
-    4 -> ["=========",
+    2 -> ["=========",
           "|    |",
           "|   " ++ " O ",
           "|   " ++ " | ",
           "|   " ++ "/ \\"]
-    5 -> ["=========",
+    1 -> ["=========",
           "|    |",
           "|   " ++ "_O ",
           "|   " ++ " | ",
           "|   " ++ "/ \\"]
-    6 -> ["=========",
+    0 -> ["=========",
           "|    |",
           "|   " ++ "_O_",
           "|   " ++ " | ",
-          "|   " ++ "/ \\"]          
+          "|   " ++ "/ \\"]  
 
-main = putStr (drawStickFigure 0)
+displayState :: Hangman -> IO ()
+displayState (Hangman word' guesses' guessesLeft)  =
+    putStrLn $ (drawStickFigure guessesLeft) ++ unlines 
+            [ ""
+            ,"Word to guess: " ++ wordWithGuesses
+            , ""
+            , "Guesses: " ++ guesses'
+            , "Guesses left: " ++ show guessesLeft
+            ]
+    where
+        gameState = Hangman word' guesses' guessesLeft
+        wordWithGuesses = blankOrChar <$> word'        
+        blankOrChar c
+            | c `elem` guesses' = c
+            | otherwise = '_'
+
+newGame :: IO Hangman
+newGame = do
+    return $ Hangman randomWord [] maxGuesses
+        
+main :: IO ()
+main = do
+    newGame >>= displayState
