3ec9ae71c2404a95f238986c266e7ca393311f86
Ghettovich <geert.boeve92@gmail.com>
2019-10-27 23:20:46 +0100

    completed hangman in haskell

--- hangman.hs
+++ hangman.hs
@@ -1,7 +1,7 @@
 data Hangman = Hangman { secret :: String, guesses:: String, guessesLeft :: Int}
 
 randomWord :: String
-randomWord = "schildpad"
+randomWord = "doktor"
 
 maxGuesses :: Int
 maxGuesses = 6
@@ -39,13 +39,53 @@ drawStickFigure x = unlines $ case x of
           "|   " ++ " | ",
           "|   " ++ "/ \\"]
 
-displayState word' guesses' guessesLeft = do
+numberOfWrongGuesses :: Hangman -> Int
+numberOfWrongGuesses (Hangman word' guesses' guessesLeft) =
+    length $ filter charNotInWord guesses'
+    where charNotInWord c = c `notElem` word'
+
+guessesInSecret :: Hangman -> Int
+guessesInSecret (Hangman word' guesses' guessesLeft) =
+    length $ filter charInGuesses word'
+    where charInGuesses c = c `elem` guesses'
+
+displayState :: Hangman -> IO ()
+displayState (Hangman word' guesses' guessesLeft) = do
+
     let stickFig = drawStickFigure guessesLeft
-    let wordWithGuesses = map(\c -> if c `elem` guesses' then c else '_')
+    let wordWithGuesses = map(\c -> if c `elem` guesses' then c else '_')      
 
     putStrLn $ stickFig
     putStrLn $ "Word to guess: " ++ wordWithGuesses word'
     putStrLn $ "Guesses: " ++ guesses'
     putStrLn $ "Guesses left: " ++ show guessesLeft
 
-main = putStr $ drawStickFigure 6
+gameLoop :: Hangman -> IO ()
+gameLoop hangman = do
+    let guessesLeft = maxGuesses - numberOfWrongGuesses hangman    
+    let guessesCorrectLength = guessesInSecret hangman
+    let secretLength = length $ secret hangman
+
+    displayState hangman { guessesLeft = guessesLeft}
+  
+    if guessesCorrectLength == secretLength then do
+        putStrLn $ "----------"
+        putStrLn $ "Hooray, you win!"
+        putStrLn $ "----------"
+    else if guessesLeft == 0 then do
+        putStrLn $ "----------"
+        putStrLn $ "Game over"
+        putStrLn $ "----------"
+    else do
+        line <- getLine
+        let guess = line
+        putStrLn $ "Guess: " ++ guess
+        gameLoop $ hangman { guesses = guesses hangman ++ guess }
+
+newGame :: IO Hangman
+newGame = do
+    return $ Hangman randomWord [] 6
+        
+main :: IO ()
+main = do
+    newGame >>= gameLoop
